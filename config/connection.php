0bfd97d4b5ac51933bfe5ba41f2ff4bc4376adbe
iamdayy <108299143+iamdayy@users.noreply.github.com>
2024-12-09 12:59:56 +0700

    Add die statement for connection error handling in connection.php

--- config/connection.php
+++ config/connection.php
@@ -10,5 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
+    die();
     die("Connection failed: " . $conn->connect_error);
 }
