4f01c1c3354d70e01f5c8230814ad179eeb0dfe6
iamdayy <108299143+iamdayy@users.noreply.github.com>
2024-12-09 12:45:05 +0700

    Comment out connection error handling in connection.php

--- config/connection.php
+++ config/connection.php
@@ -8,7 +8,7 @@ $dbname = "itsnu";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-    echo "Connection failed: " . $conn->connect_error;
-    die("Connection failed: " . $conn->connect_error);
-}
+// if ($conn->connect_error) {
+//     echo "Connection failed: " . $conn->connect_error;
+//     die("Connection failed: " . $conn->connect_error);
+// }
